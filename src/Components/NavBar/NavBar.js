8c2225bc1584f3ef0d7f0f74d5b619c2d68f70e7
Rince Mathew <rincemathewmanattu@gmail.com>
2022-08-03 22:06:41 +0530

    basic design added

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div>
+        <div className="navbar">
+            <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo" />
+            <img className='avatar' src="https://i.pinimg.com/564x/bf/a3/e1/bfa3e1784d2e94769f9475a6461cece9.jpg" alt="Avatar" />
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
