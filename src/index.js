bf53f714673b97754452cc6c9f951b7c157dc753
Rince Mathew <rincemathewmanattu@gmail.com>
2022-08-11 14:18:54 +0530

    react router dom added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import RetailedPage from './pages/DetailedPage/RetailedPage';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <App />
-  </>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<App />}/>
+    <Route path='pages' element={<RetailedPage />}/>
+    <Route path="*"
+      element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      }
+    />
+  </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/DetailedPage/RetailedPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../../Components/Banner/Banner'
+
+function detailedPage() {
+  return (
+    <div>
+      <div>detailed page</div>
+      <Banner/>
+    </div>
+  )
+}
+
+export default detailedPage
