36e7852f96328cf51e1b735541d4ce59791eb1ad
Rince Mathew <rincemathewmanattu@gmail.com>
2023-11-30 18:08:43 +0530

    new starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Banner from './Components/Banner/Banner';
-import Footer from './Components/Footer/Footer';
-import NavBar from './Components/NavBar/NavBar';
-import RowPost from './Components/RowPost/RowPost';
-import {animation, action_movies, Romance, western, fantasy, horror, comedy} from './urls'
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
-      <RowPost url={animation} title='Animation' vhSize/>
-      <RowPost url={action_movies} title='Action Movies'/>
-      <RowPost url={Romance} title='Romance'/>
-      <RowPost url={western} title='Western'/>
-      <RowPost url={fantasy} title='Fantasy'/>
-      <RowPost url={horror} title='Horror'/>
-      <RowPost url={comedy} title='Comedy'/>
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/constants/constants.js
+++
@@ -1,3 +0,0 @@
-export const baseUrl = 'https://api.themoviedb.org/3/'
-export const API_KEY = '7973743558f9afcb5f1456bcbe4963f8'
-export const imageUrl = 'https://image.tmdb.org/t/p/original/'
